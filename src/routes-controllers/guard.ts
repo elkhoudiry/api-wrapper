51d86d48ef29b6b5595fadeaf2f52fe67c64db85
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-15 22:19:24 +0200

    add basic route guards

---
+++ src/routes-controllers/guard.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { Check, guardResponse, isBoolParam, isIntParam, isValidParam } from '../utils/guard';
+
+import logging from '../utils/logging';
+
+const NAMESPACE = 'routes-controllers/guard';
+
+const check = (req: Request, res: Response, next: NextFunction) => {
+    logging.info(NAMESPACE, `sample guard check route called.`, req.params);
+
+    const checks: Check[] = [isIntParam('number', req.params.number), isValidParam('email', req.params.email), isBoolParam('verified', req.params.verified)];
+    const onPass = () => res.status(200).json({ message: 'pong' });
+
+    return guardResponse(res, checks, onPass);
+};
+
+export default { check };
