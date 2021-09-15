63f8bf5f24f100a9213785c0f3dd0014a62300b1
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-15 13:04:15 +0200

    add comment to ping check endpoint

--- src/routes-controllers/ping.ts
+++ src/routes-controllers/ping.ts
@@ -3,6 +3,7 @@ import logging from '../utils/logging';
 
 const NAMESPACE = 'routes-controller/ping';
 
+/** check ping endpoint it's used as health check */
 const check = (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'sample ping check route called.');
     return res.status(200).json({ message: 'pong' });
