6795843671d8d597418672f24cfcaa0b54117bb5
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-18 15:10:05 +0200

    feat: add sql delete warrper

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,13 +3,19 @@ import { ping, time, clients, guard } from '../routes-controllers';
 
 const router = express.Router();
 
+router.delete('/clients/delete/latest/', clients.deleteLatestClient);
+router.delete('/clients/delete/:id', clients.deleteClientById);
+
 router.post('/clients/', clients.addNewClient);
 
-router.get('/experimental/guard/*', guard.check);
 router.get('/clients/email/:email', clients.getClientByEmail);
 router.get('/clients/id/:id', clients.getClientById);
 router.get('/clients/all', clients.getAllClients);
+
+router.get('/experimental/guard/*', guard.check);
+
 router.get('/time/now', time.getTimeNow);
+
 router.get('/ping', ping.check);
 
 export = router;
