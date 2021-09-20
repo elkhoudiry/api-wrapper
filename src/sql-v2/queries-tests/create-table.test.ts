d29d7f32839e8a524abe4d9dc3c673f6f09c7b58
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-20 17:24:55 +0200

    add create table and test it

---
+++ src/sql-v2/queries-tests/create-table.test.ts
@@ -0,0 +1,86 @@
+import { QueryResult } from 'pg-mem';
+import { postgre_mem } from '../databases/postgre-mem';
+import { CreateTablePostfix, create_table, create_table_no_exist, create_table_query, SqlColumn } from '../queries/create-table';
+
+// test create table query with only 1 column name & type
+test('query builder integrity: no properties', async () => {
+    let test = 'CREATE TABLE test (id INTEGER);';
+    let test_column: SqlColumn = { name: 'id', type: 'INTEGER', notNull: false, primary: false, autoGenerate: false };
+    let postfix: CreateTablePostfix = { columns: [test_column] };
+
+    let result = create_table_query('test', postfix);
+    expect(result).toBe(test);
+});
+
+// test create table query with only 1 column name & type & not null
+test('query builder integrity: not null', async () => {
+    let test = 'CREATE TABLE test (name TEXT NOT NULL);';
+    let test_column: SqlColumn = { name: 'name', type: 'TEXT', notNull: true, primary: false, autoGenerate: false };
+    let postfix: CreateTablePostfix = { columns: [test_column] };
+
+    let result = create_table_query('test', postfix);
+
+    expect(result).toBe(test);
+});
+
+// test create table query with only 1 column name & type & primary
+test('query builder integrity: primary key', async () => {
+    let test = 'CREATE TABLE test (name TEXT PRIMARY KEY);';
+    let test_column: SqlColumn = { name: 'name', type: 'TEXT', notNull: false, primary: true, autoGenerate: false };
+    let postfix: CreateTablePostfix = { columns: [test_column] };
+
+    let result = create_table_query('test', postfix);
+
+    expect(result).toBe(test);
+});
+
+// test create table query with only 1 column name & type & auto generate as identity
+test('query builder integrity: auto generate as identity', async () => {
+    let test = 'CREATE TABLE test (name TEXT GENERATED ALWAYS AS IDENTITY);';
+    let test_column: SqlColumn = { name: 'name', type: 'TEXT', notNull: false, primary: false, autoGenerate: true };
+    let postfix: CreateTablePostfix = { columns: [test_column] };
+
+    let result = create_table_query('test', postfix);
+
+    expect(result).toBe(test);
+});
+
+// test create table query with only 1 column name & type & default value
+test('query builder integrity: default value', async () => {
+    let test = "CREATE TABLE test (name TEXT DEFAULT 'ahmed');";
+    let test_column: SqlColumn = { name: 'name', type: 'TEXT', notNull: false, primary: false, autoGenerate: false, default: 'ahmed' };
+    let postfix: CreateTablePostfix = { columns: [test_column] };
+
+    let result = create_table_query('test', postfix);
+
+    expect(result).toBe(test);
+});
+
+//test creating table with no columns should fail
+test('create new table with no columns should fail', async () => {
+    const postfix: CreateTablePostfix = { columns: [] };
+
+    const result = (await create_table(postgre_mem, 'test', postfix).response) as QueryResult;
+
+    expect(result).toBeInstanceOf(Error);
+});
+
+//test creating table should success
+test('create new table with 1 column at least should success', async () => {
+    const id_column: SqlColumn = { name: 'id', type: 'INTEGER', notNull: false, primary: true, autoGenerate: true };
+    const postfix: CreateTablePostfix = { columns: [id_column] };
+
+    const result = (await create_table(postgre_mem, 'test', postfix).response) as QueryResult;
+
+    expect(result.command).toBe('CREATE');
+});
+
+//test creating table if already exists should not fail
+test('create table if already exists should success', async () => {
+    const id_column: SqlColumn = { name: 'id', type: 'INTEGER', notNull: false, primary: true, autoGenerate: true };
+    const postfix: CreateTablePostfix = { columns: [id_column] };
+
+    const result = (await create_table_no_exist(postgre_mem, 'test', postfix).response) as QueryResult;
+
+    expect(result.command).toBe('CREATE');
+});
---
+++ src/sql-v2/queries/create-table.ts
@@ -0,0 +1,55 @@
+import { getValidSqlValue, Query, QueryExecuter } from '../databases/common';
+
+import logging from '../../utils/logging';
+
+const NAMESPACE = 'sql/queries/create-table';
+
+export type SqlColumn = {
+    name: string;
+    type: 'TEXT' | 'INTEGER' | 'FLOAT' | 'BOOLEAN';
+    notNull: boolean;
+    primary: boolean;
+    autoGenerate: boolean;
+    default?: any;
+};
+
+export type CreateTablePostfix = {
+    columns: SqlColumn[];
+};
+
+const joinColumn = (column: SqlColumn): string => {
+    let columnStr = '';
+
+    columnStr += `${column.name} `;
+    columnStr += `${column.type} `;
+    !column.notNull ? columnStr : (columnStr += `NOT NULL `);
+    !column.primary ? columnStr : (columnStr += `PRIMARY KEY `);
+    !column.autoGenerate ? columnStr : (columnStr += `GENERATED ALWAYS AS IDENTITY `);
+    !column.default ? columnStr : (columnStr += `DEFAULT ${getValidSqlValue(column.default)}`);
+
+    return columnStr.trim();
+};
+
+const createTableQueryBuilder = (postfix: CreateTablePostfix): string => {
+    return postfix.columns.map(joinColumn).join();
+};
+
+const create_table_query = (table: string, postfix: CreateTablePostfix): string => {
+    const options = createTableQueryBuilder(postfix);
+
+    return `CREATE TABLE ${table} (${options});`;
+};
+
+const create_table_no_exist = <T>(executer: QueryExecuter<T>, table: string, postfix: CreateTablePostfix): Query<T> => {
+    return create_table(executer, `IF NOT EXISTS ${table}`, postfix);
+};
+
+const create_table = <T>(executer: QueryExecuter<T>, table: string, postfix: CreateTablePostfix): Query<T> => {
+    const query = create_table_query(table, postfix);
+
+    logging.info(NAMESPACE, `query: ${query}`);
+
+    return { response: executer.execute(query) };
+};
+
+export { create_table, create_table_query, create_table_no_exist };
